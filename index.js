07f54158d8c41cfdc86bf7c92d33f385c85e1ea6
Ethanx0813 <ankityadav770587@gmail.com>
2023-10-24 20:12:22 +0530

    first assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const number = prompt("Enter a valid number:");
+if ( number <= 0) {
+    console.log("Error: Please enter a positive number.");
+  } else {
+    console.log(`The first 10 multiples of  are:`);
+    for (let i = 1; i <= 10; i++) {
+      const multiple = number * i;
+      console.log(multiple);
+    }
+  }
+
+  
+  
+  
+  
