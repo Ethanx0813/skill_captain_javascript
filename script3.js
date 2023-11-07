89659af95664ac69d0932427c3fe1026f1178f32
Ethanx0813 <ankityadav770587@gmail.com>
2023-11-07 14:52:03 +0530

    Object assignmebt

---
+++ script3.js
@@ -0,0 +1,16 @@
+
+let book = {
+    title: 'Jungle Book',
+    author: 'Rudyard krippling',
+    year: 1925,
+    isAvailable: true
+};
+console.log(book.title)// accessing obbjects using dot notation
+console.log(book.author)
+console.log(book.year)
+console.log(book.isAvailable)
+// accessing objects using bracket notation
+console.log(book['title'])
+console.log(book['author'])
+console.log(book['year'])
+console.log(book['year'])
